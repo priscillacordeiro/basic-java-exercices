b7048774e7b6438ceb46413a81cde5fa1159278c
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2020-02-16 10:54:36 -0300

    ForTest answer

---
+++ src/recode/basicsyntax/ForTest.java
@@ -0,0 +1,19 @@
+package recode.basicsyntax;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+
+/* 		Estruturas de Controle
+ * 
+ *		1.	Criar uma nova classe Java com o nome ForTest. Neste arquivo, inserir as seguintes funcionalidades no método main:  
+ *		a.	Contar de 0 até 10 e mostrar o valor de cada interação no console.
+ */
+		
+		for(int count = 0; count <= 10; count++) {
+			System.out.println(count);
+		}
+
+	}
+
+}
